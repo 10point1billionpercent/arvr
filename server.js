e6c69ad50ec924392b8d97a9427fe84c0958f6d0
LOl <lol262711@gmail.com>
2026-01-21 11:29:25 +0530

    hi

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ADD THIS TEST ROUTE:
+app.get('/', (req, res) => {
+    res.send('<h1>Server is working!</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    // Corrected to http://
+    console.log(`Server running on http://localhost:${PORT}`);
+});
